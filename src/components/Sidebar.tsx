577d7c7c21d9dfa236d5a30b7d13aa8ea0931a83
LucasTracker <lucasgabriel3490@gmail.com>
2022-06-26 13:54:55 -0300

    app da Rockeatseat

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Lesson } from "./Lesson";
+import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
+
+export function Sidebar(){
+  const {data} = useGetLessonsQuery();
+  return(
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de Aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map(lesson => {
+          return (
+            <Lesson 
+              key={lesson.id}
+              title={lesson.title}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType} slug={lesson.slug} 
+              id={lesson.id}            />
+          )
+        })}
+      </div>
+    </aside>
+  )
+}
